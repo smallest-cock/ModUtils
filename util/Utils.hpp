b607ce7e704c55f4dab834b58381c7608bcfbee8
smallest-cock <soccerstarslegend420@gmail.com>
2025-04-01 18:10:13 -0700

    add fix

--- util/Utils.hpp
+++ util/Utils.hpp
@@ -5,6 +5,12 @@
 
 namespace Format
 {
+	constexpr std::array<char, 53> letters =
+	{
+		'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z',
+		'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z',' '
+	};
+
 	void construct_label(const std::vector<int>& codes, std::string& out_str);
 
 	std::string ToASCIIString(std::string str);

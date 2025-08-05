9934c26da0b6685318b2de9b87bc4244e1293f32
smallest-cock <soccerstarslegend420@gmail.com>
2025-08-05 00:57:23 -0700

    add centering text functions

--- gui/GuiTools.cpp
+++ gui/GuiTools.cpp
@@ -1,192 +1,198 @@
+#include "BakkesmodPluginTemplate/IMGUI/imgui.h"
 #include "pch.h"
 #include "GuiTools.hpp"
 #include "../util/Utils.hpp"
 
-
 namespace GUI
 {
-	namespace Colors
+namespace Colors
+{
+const ImVec4 White         = {1, 1, 1, 1};
+const ImVec4 Red           = {1, 0, 0, 1};
+const ImVec4 Green         = {0, 1, 0, 1};
+const ImVec4 Blue          = {0, 0, 1, 1};
+const ImVec4 Yellow        = {1, 1, 0, 1};
+const ImVec4 BlueGreen     = {0, 1, 1, 1};
+const ImVec4 Pinkish       = {1, 0, 1, 1};
+const ImVec4 LightBlue     = {0.5f, 0.5f, 1.0f, 1.0f};
+const ImVec4 LightRed      = {1.0f, 0.4f, 0.4f, 1.0f};
+const ImVec4 LightGreen    = {0.5f, 1.0f, 0.5f, 1.0f};
+const ImVec4 DarkGreen     = {0.0f, 0.5f, 0.0f, 1.0f};
+const ImVec4 ForestGreen   = {0.3f, 0.5f, 0.3f, 1.0f};
+const ImVec4 RealDarkGreen = {0.1f, 0.3f, 0.1f, 1.0f};
+const ImVec4 Gray          = {0.4f, 0.4f, 0.4f, 1.0f};
+const ImVec4 LightOrange   = {1.0f, 0.6f, 0.2f, 1.0f};
+const ImVec4 VividOrange   = {1.0f, 0.4f, 0.0f, 1.0f};
+} // namespace Colors
+
+void open_link(const char* url)
+{
+	std::wstring wide_url = StringUtils::ToWideString(url);
+	ShellExecute(NULL, L"open", wide_url.c_str(), NULL, NULL, SW_SHOWNORMAL);
+}
+
+void open_link(const wchar_t* url) { ShellExecute(NULL, L"open", url, NULL, NULL, SW_SHOWNORMAL); }
+
+// old bummy shit
+void ClickableLink(const char* label, const char* url, const ImVec4& textColor, ImVec2 size)
+{
+	// default size of selectable is just size of label text
+	if (size.x == 0 && size.y == 0)
 	{
-		const ImVec4 White =				{ 1, 1, 1, 1 };
-		const ImVec4 Red =					{ 1, 0, 0, 1 };
-		const ImVec4 Green =				{ 0, 1, 0, 1 };
-		const ImVec4 Blue =					{ 0, 0, 1, 1 };
-
-		const ImVec4 Yellow =				{ 1, 1, 0, 1 };
-		const ImVec4 BlueGreen =			{ 0, 1, 1, 1 };
-		const ImVec4 Pinkish =				{ 1, 0, 1, 1 };
-
-		const ImVec4 LightBlue =			{ 0.5f, 0.5f, 1.0f, 1.0f };
-		const ImVec4 LightRed =				{ 1.0f, 0.4f, 0.4f, 1.0f };
-		const ImVec4 LightGreen =			{ 0.5f, 1.0f, 0.5f, 1.0f };
-
-		const ImVec4 DarkGreen =			{ 0.0f, 0.5f, 0.0f, 1.0f };
-		const ImVec4 ForestGreen =			{ 0.3f, 0.5f, 0.3f, 1.0f };
-		const ImVec4 RealDarkGreen =		{ 0.1f, 0.3f, 0.1f, 1.0f };
-
-		const ImVec4 Gray =					{ 0.4f, 0.4f, 0.4f, 1.0f };
+		size = ImGui::CalcTextSize(label);
 	}
 
+	ImGui::PushStyleColor(ImGuiCol_Text, textColor);
 
-	void open_link(const char* url)
+	if (ImGui::Selectable(label, false, ImGuiSelectableFlags_None, size))
 	{
-		std::wstring wide_url = StringUtils::ToWideString(url);
-		ShellExecute(NULL, L"open", wide_url.c_str(), NULL, NULL, SW_SHOWNORMAL);
+		open_link(url);
 	}
 
-	void open_link(const wchar_t* url)
+	ImGui::PopStyleColor();
+
+	if (ImGui::IsItemHovered())
 	{
-		ShellExecute(NULL, L"open", url, NULL, NULL, SW_SHOWNORMAL);
+		ImGui::SetMouseCursor(ImGuiMouseCursor_Hand);
+		ImGui::SetTooltip("%s", url);
 	}
+}
 
-
-	// old bummy shit
-	void ClickableLink(const char* label, const char* url, const ImVec4& textColor, ImVec2 size)
+void Spacing(int amount)
+{
+	for (int i = 0; i < amount; i++)
 	{
-		// default size of selectable is just size of label text
-		if (size.x == 0 && size.y == 0)
-		{
-			size = ImGui::CalcTextSize(label);
-		}
-
-		ImGui::PushStyleColor(ImGuiCol_Text, textColor);
-
-		if (ImGui::Selectable(label, false, ImGuiSelectableFlags_None, size))
-		{
-			open_link(url);
-		}
-
-		ImGui::PopStyleColor();
-		
-		if (ImGui::IsItemHovered())
-		{
-			ImGui::SetMouseCursor(ImGuiMouseCursor_Hand);
-			ImGui::SetTooltip("%s", url);
-		}
+		ImGui::Spacing();
 	}
+}
 
+void SameLineSpacing_relative(float horizontalSpacingPx)
+{
+	ImGui::SameLine();
+	ImGui::SetCursorPosX(ImGui::GetCursorPosX() + horizontalSpacingPx);
+}
 
-	void Spacing(int amount)
-	{
-		for (int i = 0; i < amount; i++) {
-			ImGui::Spacing();
-		}
-	}
+void SameLineSpacing_absolute(float horizontalSpacingPx)
+{
+	ImGui::SameLine();
+	ImGui::SetCursorPosX(horizontalSpacingPx);
+}
 
-	void SameLineSpacing_relative(float horizontalSpacingPx)
-	{
-		ImGui::SameLine();
-		ImGui::SetCursorPosX(ImGui::GetCursorPosX() + horizontalSpacingPx);
-	}
+void ToolTip(const char* tip)
+{
+	if (ImGui::IsItemHovered())
+		ImGui::SetTooltip("%s", tip);
+}
 
-	void SameLineSpacing_absolute(float horizontalSpacingPx)
-	{
-		ImGui::SameLine();
-		ImGui::SetCursorPosX(horizontalSpacingPx);
-	}
+void centerTextX(const char* text, float offsetCorrection)
+{
+	ImVec2 text_size        = ImGui::CalcTextSize(text);
+	float  horizontalOffset = (ImGui::GetContentRegionAvail().x - text_size.x) * 0.5f;
+	ImGui::SetCursorPosX(horizontalOffset + offsetCorrection);
+	ImGui::TextUnformatted(text);
+}
 
-	void ToolTip(const char* tip)
-	{
-		if (ImGui::IsItemHovered())
-			ImGui::SetTooltip("%s", tip);
-	}
+void centerTextColoredX(const ImVec4& col, const char* text, float offsetCorrection)
+{
+	ImVec2 text_size        = ImGui::CalcTextSize(text);
+	float  horizontalOffset = (ImGui::GetContentRegionAvail().x - text_size.x) * 0.5f;
+	ImGui::SetCursorPosX(horizontalOffset + offsetCorrection);
+	ImGui::TextColored(col, "%s", text);
+}
 
-
-	void SettingsHeader(const char* id, const char* pluginVersion, const ImVec2& size, bool showBorder)
-	{
-		if (ImGui::BeginChild(id, size, showBorder))
-		{
-			Spacing(4);
-
-			ImGui::TextColored(Colors::Pinkish, "Plugin made by SSLow");
-
-			Spacing(3);
-
-			ImGui::Text("%s", pluginVersion);
-			ImGui::Separator();
-		}
-		ImGui::EndChild();
-	}
-
-	void OldSettingsFooter(const char* id, const ImVec2& size, bool showBorder)
-	{
-		if (ImGui::BeginChild(id, size, showBorder))
-		{
-			constexpr const char* linkText = "Need help? Join the Discord";
-
-			// center the the cursor position horizontally
-			ImVec2 text_size = ImGui::CalcTextSize(linkText);
-			float horizontal_offset = (ImGui::GetContentRegionAvail().x - text_size.x) / 2;
-			horizontal_offset -= 50;	// seems slightly shifted to the right, so subtract 50 to compensate
-			ImGui::SetCursorPosX(horizontal_offset);
-
-			// make link a lil more visible
-			ImGui::SetWindowFontScale(1.3);
-
-			ClickableLink(linkText, "https://discord.gg/tHZFsMsvDU", Colors::Yellow, text_size);
-		}
-		ImGui::EndChild();
-	}
-
-
-	void alt_settings_header(const char* text, const char* plugin_version, const ImVec4& text_color)
+void SettingsHeader(const char* id, const char* pluginVersion, const ImVec2& size, bool showBorder)
+{
+	if (ImGui::BeginChild(id, size, showBorder))
 	{
 		Spacing(4);
 
-		ImGui::TextColored(text_color, "%s", text);
+		ImGui::TextColored(Colors::Pinkish, "Plugin made by SSLow");
 
 		Spacing(3);
 
-		ImGui::Text("%s", plugin_version);
-
-		plugin_update_message();
-
+		ImGui::Text("%s", pluginVersion);
 		ImGui::Separator();
 	}
+	ImGui::EndChild();
+}
 
-	void alt_settings_footer(const char* text, const char* url, const ImVec4& text_color)
+void OldSettingsFooter(const char* id, const ImVec2& size, bool showBorder)
+{
+	if (ImGui::BeginChild(id, size, showBorder))
 	{
-		ImGui::SetWindowFontScale(1.3);	// make link a lil more visible
+		constexpr const char* linkText = "Need help? Join the Discord";
 
 		// center the the cursor position horizontally
-		ImVec2 text_size = ImGui::CalcTextSize(text);
-		float horizontal_offset = (ImGui::GetContentRegionAvail().x - text_size.x) / 2;
-		horizontal_offset -= 50;	// seems slightly shifted to the right, so subtract 50 to compensate
+		ImVec2 text_size         = ImGui::CalcTextSize(linkText);
+		float  horizontal_offset = (ImGui::GetContentRegionAvail().x - text_size.x) / 2;
+		horizontal_offset -= 50; // seems slightly shifted to the right, so subtract 50 to compensate
 		ImGui::SetCursorPosX(horizontal_offset);
 
-		ClickableLink(text, url, text_color, text_size);
+		// make link a lil more visible
+		ImGui::SetWindowFontScale(1.3);
 
-		ImGui::SetWindowFontScale(1);	// undo font scale modification, so it doesnt affect the rest of the UI
+		ClickableLink(linkText, "https://discord.gg/tHZFsMsvDU", Colors::Yellow, text_size);
+	}
+	ImGui::EndChild();
+}
+
+void alt_settings_header(const char* text, const char* plugin_version, const ImVec4& text_color)
+{
+	Spacing(4);
+
+	ImGui::TextColored(text_color, "%s", text);
+
+	Spacing(3);
+
+	ImGui::Text("%s", plugin_version);
+
+	plugin_update_message();
+
+	ImGui::Separator();
+}
+
+void alt_settings_footer(const char* text, const char* url, const ImVec4& text_color)
+{
+	ImGui::SetWindowFontScale(1.3); // make link a lil more visible
+
+	// center the the cursor position horizontally
+	ImVec2 text_size         = ImGui::CalcTextSize(text);
+	float  horizontal_offset = (ImGui::GetContentRegionAvail().x - text_size.x) / 2;
+	horizontal_offset -= 50; // seems slightly shifted to the right, so subtract 50 to compensate
+	ImGui::SetCursorPosX(horizontal_offset);
+
+	ClickableLink(text, url, text_color, text_size);
+
+	ImGui::SetWindowFontScale(1); // undo font scale modification, so it doesnt affect the rest of the UI
+}
+
+void plugin_update_message()
+{
+	PluginUpdates::PluginUpdateResponse update_status;
+
+	{
+		std::lock_guard<std::mutex> lock(PluginUpdates::update_mutex);
+		update_status = PluginUpdates::update_response;
 	}
 
-	void plugin_update_message()
+	if (update_status.out_of_date)
 	{
-		PluginUpdates::PluginUpdateResponse update_status;
+		GUI::SameLineSpacing_relative(20);
 
+		ImGui::TextColored(GUI::Colors::Red, "<------ PLUGIN IS OUT OF DATE!");
+		if (ImGui::IsItemHovered())
 		{
-			std::lock_guard<std::mutex> lock(PluginUpdates::update_mutex);
-			update_status = PluginUpdates::update_response;
+			ImGui::SetTooltip(
+			    "Using an outdated version can cause features to work incorrectly or even crash the game!\n\n"
+			    "Unfortunately there's no built in auto-update system for plugins that aren't on the BakkesPlugins website :(\n\n"
+			    "So just follow the install steps on the latest GitHub release to update manually\t(it's very easy)");
 		}
+		GUI::SameLineSpacing_relative(20);
+		ImGui::Text("The latest version is v%s:", update_status.latest_version.c_str());
 
-		if (update_status.out_of_date)
-		{
-			GUI::SameLineSpacing_relative(20);
+		GUI::SameLineSpacing_relative(10);
 
-			ImGui::TextColored(GUI::Colors::Red, "<------ PLUGIN IS OUT OF DATE!");
-			if (ImGui::IsItemHovered())
-			{
-				ImGui::SetTooltip(
-					"Using an outdated version can cause features to work incorrectly or even crash the game!\n\n"
-					"Unfortunately there's no built in auto-update system for plugins that aren't on the BakkesPlugins website :(\n\n"
-					"So just follow the install steps on the latest GitHub release to update manually\t(it's very easy)"
-				);
-			}
-			GUI::SameLineSpacing_relative(20);
-			ImGui::Text("The latest version is v%s:", update_status.latest_version.c_str());
-
-			GUI::SameLineSpacing_relative(10);
-
-			GUI::ClickableLink("Update to latest version", update_status.release_url.c_str(), GUI::Colors::Green);
-		}
+		GUI::ClickableLink("Update to latest version", update_status.release_url.c_str(), GUI::Colors::Green);
 	}
 }
+} // namespace GUI
--- gui/GuiTools.hpp
+++ gui/GuiTools.hpp
@@ -1,150 +1,141 @@
 #pragma once
 #include "pch.h"
 
-
 namespace GUI
 {
-	void open_link(const char* url);
-	void open_link(const wchar_t* url);
+void open_link(const char* url);
+void open_link(const wchar_t* url);
 
-	void ClickableLink(const char* label, const char* url, const ImVec4& textColor = ImVec4(1, 1, 1, 1), ImVec2 size = ImVec2(0, 0));
+void ClickableLink(const char* label, const char* url, const ImVec4& textColor = ImVec4(1, 1, 1, 1), ImVec2 size = ImVec2(0, 0));
 
-	void Spacing(int amount = 1);
-	void SameLineSpacing_relative(float horizontalSpacingPx);
-	void SameLineSpacing_absolute(float horizontalSpacingPx);
-	void ToolTip(const char* tip);
+void Spacing(int amount = 1);
+void SameLineSpacing_relative(float horizontalSpacingPx);
+void SameLineSpacing_absolute(float horizontalSpacingPx);
+void ToolTip(const char* tip);
+void centerTextX(const char* text, float offsetCorrection = 0.0f);
+void centerTextColoredX(const ImVec4& col, const char* text, float offsetCorrection = 0.0f);
 
-	// scoped version of BeginChild/EndChild
-	// Usage: GUI::ScopedChild c{ "SomeLabel", ... };
-	struct ScopedChild
+// scoped version of BeginChild/EndChild
+// Usage: GUI::ScopedChild c{ "SomeLabel", ... };
+struct ScopedChild
+{
+	ScopedChild(const char* str_id, const ImVec2& size = ImVec2(0, 0), bool border = false, ImGuiWindowFlags flags = 0)
 	{
-		ScopedChild(const char* str_id, const ImVec2& size = ImVec2(0, 0), bool border = false, ImGuiWindowFlags flags = 0)
-		{
-			ImGui::BeginChild(str_id, size, border, flags);
-		}
-
-		~ScopedChild() { ImGui::EndChild(); }
-	};
-
-	// scoped version of PushID/PopID
-	// Usage: GUI::ScopedID id{ "SomeID" };
-	struct ScopedID
-	{
-		ScopedID(const char* str_id)								{ ImGui::PushID(str_id); }
-		ScopedID(const char* str_id_begin, const char* str_id_end)	{ ImGui::PushID(str_id_begin, str_id_end); }
-		ScopedID(const void* ptr_id)								{ ImGui::PushID(ptr_id); }
-		ScopedID(int id)											{ ImGui::PushID(id); }
-
-		~ScopedID() { ImGui::PopID(); }
-	};
-
-	// scoped version of Indent/Unindent
-    // Usage: GUI::ScopedIndent indent{ 20.0f };
-	struct ScopedIndent
-    {
-	    const float m_width;
-
-	    ScopedIndent(float width = 0.0f) : m_width(width) { ImGui::Indent(width); }
-	    ~ScopedIndent() { ImGui::Unindent(m_width); }
-	};
-
-
-	namespace Colors
-	{
-		struct Color
-		{
-			float r, g, b, a;
-
-			Color() : r(1.0f), g(1.0f), b(1.0f), a(1.0f) {}
-			
-			// Initialize with float values
-			Color(float r, float g, float b, float a) : r(r), g(g), b(b), a(a) {}
-
-			Color(const ImVec4& color) : r(color.w), g(color.x), b(color.y), a(color.z) {}
-
-			// Initialize with an FLinearColor
-			Color(const FLinearColor& color) : r(color.R), g(color.G), b(color.B), a(color.A) {}
-
-			// Initialize with a int32_t color (0xAARRGGBB format)
-			Color(int32_t color)
-			{
-				a = ((color >> 24) & 0xFF)	/ 255.0f;	// Extract and normalize Alpha
-				r = ((color >> 16) & 0xFF)	/ 255.0f;	// Extract and normalize Red
-				g = ((color >> 8) & 0xFF)	/ 255.0f;	// Extract and normalize Green
-				b = (color & 0xFF)			/ 255.0f;	// Extract and normalize Blue
-			}
-
-			// Initialize with an FColor
-			Color(const FColor& color)
-			{
-				r = color.R / 255.0f; // Normalize Red
-				g = color.G / 255.0f; // Normalize Green
-				b = color.B / 255.0f; // Normalize Blue
-				a = color.A / 255.0f; // Normalize Alpha
-			}
-
-
-
-			ImVec4 GetImGuiColor() const
-			{
-				return ImVec4{ r, g, b, a };
-			}
-
-			FLinearColor GetLinearColor() const
-			{
-				return FLinearColor{ r, g, b, a };
-			}
-
-			int32_t GetIntColor() const
-			{
-				FColor col = GetFColor();
-
-				return	(static_cast<int32_t>(col.A) << 24)	|
-						(static_cast<int32_t>(col.R) << 16)	|
-						(static_cast<int32_t>(col.G) << 8)	|
-						static_cast<int32_t>(col.B);
-			}
-
-			FColor GetFColor() const
-			{
-				FColor col;
-
-				col.R = static_cast<uint8_t>(std::round(r * 255.0f));
-				col.G = static_cast<uint8_t>(std::round(g * 255.0f));
-				col.B = static_cast<uint8_t>(std::round(b * 255.0f));
-				col.A = static_cast<uint8_t>(std::round(a * 255.0f));
-
-				return col;
-			}
-		};
-
-
-		extern const ImVec4 White;
-		extern const ImVec4 Red;
-		extern const ImVec4 Green;
-		extern const ImVec4 Blue;
-		
-		extern const ImVec4 Yellow;
-		extern const ImVec4 BlueGreen;
-		extern const ImVec4 Pinkish;
-		
-		extern const ImVec4 LightBlue;
-		extern const ImVec4 LightRed;
-		extern const ImVec4 LightGreen;
-
-		extern const ImVec4 DarkGreen;
-		extern const ImVec4 ForestGreen;
-		extern const ImVec4 RealDarkGreen;
-
-		extern const ImVec4 Gray;
+		ImGui::BeginChild(str_id, size, border, flags);
 	}
 
+	~ScopedChild() { ImGui::EndChild(); }
+};
 
-	void SettingsHeader(const char* id, const char* pluginVersion, const ImVec2& size, bool showBorder = false);
-	void OldSettingsFooter(const char* id, const ImVec2& size, bool showBorder = false);
+// scoped version of PushID/PopID
+// Usage: GUI::ScopedID id{ "SomeID" };
+struct ScopedID
+{
+	ScopedID(const char* str_id) { ImGui::PushID(str_id); }
+	ScopedID(const char* str_id_begin, const char* str_id_end) { ImGui::PushID(str_id_begin, str_id_end); }
+	ScopedID(const void* ptr_id) { ImGui::PushID(ptr_id); }
+	ScopedID(int id) { ImGui::PushID(id); }
 
-	void alt_settings_header(const char* text, const char* plugin_version, const ImVec4& text_color = Colors::Pinkish);
-	void alt_settings_footer(const char* text, const char* url, const ImVec4& text_color = Colors::Yellow);
+	~ScopedID() { ImGui::PopID(); }
+};
 
-	void plugin_update_message();
-}
+// scoped version of Indent/Unindent
+// Usage: GUI::ScopedIndent indent{ 20.0f };
+struct ScopedIndent
+{
+	const float m_width;
+
+	ScopedIndent(float width = 0.0f) : m_width(width) { ImGui::Indent(width); }
+	~ScopedIndent() { ImGui::Unindent(m_width); }
+};
+
+namespace Colors
+{
+struct Color
+{
+	float r, g, b, a;
+
+	Color() : r(1.0f), g(1.0f), b(1.0f), a(1.0f) {}
+
+	// Initialize with float values
+	Color(float r, float g, float b, float a) : r(r), g(g), b(b), a(a) {}
+
+	Color(const ImVec4& color) : r(color.w), g(color.x), b(color.y), a(color.z) {}
+
+	// Initialize with an FLinearColor
+	Color(const FLinearColor& color) : r(color.R), g(color.G), b(color.B), a(color.A) {}
+
+	// Initialize with a int32_t color (0xAARRGGBB format)
+	Color(int32_t color)
+	{
+		a = ((color >> 24) & 0xFF) / 255.0f; // Extract and normalize Alpha
+		r = ((color >> 16) & 0xFF) / 255.0f; // Extract and normalize Red
+		g = ((color >> 8) & 0xFF) / 255.0f;  // Extract and normalize Green
+		b = (color & 0xFF) / 255.0f;         // Extract and normalize Blue
+	}
+
+	// Initialize with an FColor
+	Color(const FColor& color)
+	{
+		r = color.R / 255.0f; // Normalize Red
+		g = color.G / 255.0f; // Normalize Green
+		b = color.B / 255.0f; // Normalize Blue
+		a = color.A / 255.0f; // Normalize Alpha
+	}
+
+	ImVec4 GetImGuiColor() const { return ImVec4{r, g, b, a}; }
+
+	FLinearColor GetLinearColor() const { return FLinearColor{r, g, b, a}; }
+
+	int32_t GetIntColor() const
+	{
+		FColor col = GetFColor();
+
+		return (static_cast<int32_t>(col.A) << 24) | (static_cast<int32_t>(col.R) << 16) | (static_cast<int32_t>(col.G) << 8) |
+		       static_cast<int32_t>(col.B);
+	}
+
+	FColor GetFColor() const
+	{
+		FColor col;
+
+		col.R = static_cast<uint8_t>(std::round(r * 255.0f));
+		col.G = static_cast<uint8_t>(std::round(g * 255.0f));
+		col.B = static_cast<uint8_t>(std::round(b * 255.0f));
+		col.A = static_cast<uint8_t>(std::round(a * 255.0f));
+
+		return col;
+	}
+};
+
+extern const ImVec4 White;
+extern const ImVec4 Red;
+extern const ImVec4 Green;
+extern const ImVec4 Blue;
+
+extern const ImVec4 Yellow;
+extern const ImVec4 BlueGreen;
+extern const ImVec4 Pinkish;
+
+extern const ImVec4 LightBlue;
+extern const ImVec4 LightRed;
+extern const ImVec4 LightGreen;
+
+extern const ImVec4 DarkGreen;
+extern const ImVec4 ForestGreen;
+extern const ImVec4 RealDarkGreen;
+
+extern const ImVec4 Gray;
+
+extern const ImVec4 LightOrange;
+extern const ImVec4 VividOrange;
+} // namespace Colors
+
+void SettingsHeader(const char* id, const char* pluginVersion, const ImVec2& size, bool showBorder = false);
+void OldSettingsFooter(const char* id, const ImVec2& size, bool showBorder = false);
+
+void alt_settings_header(const char* text, const char* plugin_version, const ImVec4& text_color = Colors::Pinkish);
+void alt_settings_footer(const char* text, const char* url, const ImVec4& text_color = Colors::Yellow);
+
+void plugin_update_message();
+} // namespace GUI
